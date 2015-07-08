58b71bf87e2903f404f5a785c0b50283c104db00
Алексей <waw555@yandex.ru>
2015-07-08 19:20:54 +0500

    Удаление лишнего

--- forgotPassword.php
+++ forgotPassword.php
@@ -1,13 +1,4 @@
 <?php
-
-/* +**********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.1
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
- * ********************************************************************************** */
 require_once 'include/utils/utils.php';
 require_once 'include/utils/VtlibUtils.php';
 require_once 'modules/Emails/class.phpmailer.php';
@@ -16,21 +7,17 @@ require_once 'modules/Vtiger/helpers/ShortURL.php';
 global $adb;
 $adb = PearDatabase::getInstance();
 
-// SalesPlatform.ru begin
 if (isset($_REQUEST['username']) && isset($_REQUEST['email'])) {
     $username = vtlib_purify($_REQUEST['username']);
 //if (isset($_REQUEST['user_name']) && isset($_REQUEST['emailId'])) {
 //    $username = vtlib_purify($_REQUEST['user_name']);
-// SalesPlatform.ru end
     $result = $adb->pquery('select email1 from vtiger_users where user_name= ? ', array($username));
     if ($adb->num_rows($result) > 0) {
         $email = $adb->query_result($result, 0, 'email1');
     }
 
-    // SalesPlatform.ru begin
     if (vtlib_purify($_REQUEST['email']) == $email) {
     //if (vtlib_purify($_REQUEST['emailId']) == $email) {
-    // SalesPlatform.ru end
         $time = time();
         $options = array(
             'handler_path' => 'modules/Users/handlers/ForgotPassword.php',
@@ -44,7 +31,7 @@ if (isset($_REQUEST['username']) && isset($_REQUEST['email'])) {
             )
         );
         $trackURL = Vtiger_ShortURL_Helper::generateURL($options);
-        // SalesPlatform.ru begin
+
         $content = 'Уважамаемый Пользователь,<br><br>
                             Вы запросили смену пароля в системе ООО "Евролок Проект".<br>
                             Для создания нового пароля, нажмите на <a target="_blank" href=' . $trackURL . '>ссылку</a>.
@@ -61,12 +48,10 @@ if (isset($_REQUEST['username']) && isset($_REQUEST['email'])) {
 //		            Regards,<br>
 //		            VtigerCRM Open source Support Team.<br>' ;
 //                $mail = new PHPMailer();
-        // SalesPlatform.ru end
 
-        // SalesPlatform.ru begin
         setMailerProperties($mail, 'Восстановление пароля', $content, null, $username, $email);
         //setMailerProperties($mail, 'Request : ForgotPassword - vtigercrm', $content, 'support@vtiger.com', $username, $email);
-        // SalesPlatform.ru end
+
 
         $status = MailSend($mail);
         if ($status === 1)
